5ba3db07777472216d81816a36c2e9f246a0f7f5
Feo <jjzxcc@gmail.com>
2015-03-17 17:14:51 +0800

    Init.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ConwayGame::Application.config.secret_token = '541630bfc14e8c1cb226185f999f86aece032cf1e22fad921746ff6108a786d0cf1cbf877c47ccec876f40b4c3c43c3cf4d0c2f2861a25b9c40a4884ffb83af8'
